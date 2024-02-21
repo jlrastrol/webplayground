4c6324f6e31847d774c6e31d80fbb0f3c1db1ebc
Juan Luis Rastrollo Guerrero <juanlurasgue06@gmail.com>
2024-02-21 16:18:38 +0100

    View CRUD with CBV: CreateView

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from .views import PageListView, PageDetailView, PageCreate
 
-urlpatterns = [
-    path('', views.pages, name='pages'),
-    path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
-]
+pages_patterns = ([
+    path('', PageListView.as_view(), name='pages'),
+    path('<int:pk>/<slug:page_slug>/', PageDetailView.as_view(), name='page'),
+    path('create/', PageCreate.as_view(), name='create'),
+], 'pages')
