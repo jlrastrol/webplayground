1dfd532d4962221a523c1a317c1ec66945131fec
Juan Luis Rastrollo Guerrero <juanlurasgue06@gmail.com>
2024-02-22 09:37:47 +0100

    View CRUD with CBV: UpdateView and DeleteView

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,8 +1,9 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from .models import Page
+from .forms import PageForm
 
 # Create your views here.
 class PageListView(ListView):
@@ -13,5 +14,16 @@ class PageDetailView(DetailView):
 
 class PageCreate(CreateView):
     model = Page
-    fields = ["title","content","order"]
+    form_class = PageForm
+    success_url = reverse_lazy("pages:pages")
+
+class PageUpdate(UpdateView):
+    model = Page
+    form_class = PageForm
+    template_name_suffix = "_update_form"
+    def get_success_url(self) -> str:
+        return reverse_lazy("pages:update", args=[self.object.id])+'?ok'
+    
+class PageDelete(DeleteView):
+    model = Page
     success_url = reverse_lazy("pages:pages")
