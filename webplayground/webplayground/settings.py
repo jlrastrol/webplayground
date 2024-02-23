f1293e9df361f6945688b25995d3defb5074167c
Juan Luis Rastrollo Guerrero <juanlurasgue06@gmail.com>
2024-02-23 09:06:21 +0100

    New app Registration

--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration', # Es importante ponerla primero para darle prioridad
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Auth redirects
+LOGIN_REDIRECT_URL = 'pages:pages'
+LOGOUT_REDIRECT_URL = 'home'
